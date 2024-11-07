dd8e21a0297bc7717dae287d53c6a4dba3536d26
André Matthies <matthiez@gmail.com>
2024-11-07 11:14:20 +0100

    implement Voice.hangup

--- lib/seven_api/resources/voice.rb
+++ lib/seven_api/resources/voice.rb
@@ -7,8 +7,17 @@ module SevenApi::Resources
   class Voice < SevenApi::Resource
     @endpoint = SevenApi::Endpoint::VOICE
     @http_methods = {
+      :hangup => :post,
       :send => :post,
     }
+
+    # This endpoint ends an active call. Only calls with the status in-progress can be ended.
+    # read more: https://docs.seven.io/en/rest-api/endpoints/voice#hangup-a-call
+    # @param call_id [String]
+    # @return Hash
+    def hangup(call_id)
+      request({}, {}, "/#{call_id}/hangup")
+    end
 
     # Convert text to speech, call number and read voice message out loud.
     # read more: https://docs.seven.io/en/rest-api/endpoints/voice#send-voice-call
